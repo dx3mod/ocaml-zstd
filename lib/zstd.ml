a32416e32d5d20ac62d33eb73ff02717ed37807c
dx3mod <kxyzd@vk.com>
2026-09-14 15:11:49 +0300

    Add version function

--- lib/zstd.ml
+++ lib/zstd.ml
@@ -7,4 +7,17 @@ module Compressor = Compressor
 module Decompressor = Decompressor
 module Dictionary = Dictionary
 module Io_buffer = Io_buffer
+
+(** {2 Miscellaneous} *)
+
+(** [version ()]
+
+    Returns the version of the linked libzstd C library as a triple
+    [(major, minor, patch)].*)
+let version () =
+  let v = Bindings.version () in
+  (v / 10_000, v / 100 mod 100, v mod 100)
+
+(** {2 Internals} *)
+
 module Bindings_intf = Bindings
