cca090a4de082b21435b9e1172aa081247265004
dx3mod <kxyzd@vk.com>
2026-09-15 16:20:55 +0300

    Big refactoring

--- lib/io_buffer.ml
+++ lib/io_buffer.ml
@@ -1,10 +1,16 @@
-type t = { buffer : Bstr.t; mutable position : int; mutable size : int }
+type t = { buffer : Bstr.t; mutable position : int; size : int }
 
-let make ?size ?position buffer =
-  {
-    buffer;
-    size = Option.(value ~default:(Bstr.length buffer) size);
-    position = Option.(value ~default:0 position);
-  }
+let create size = { buffer = Bstr.create size; position = 0; size }
+
+let make ?pos ?size buffer =
+  let size = Option.value ~default:(Bstr.length buffer) size in
+  let position = Option.value ~default:0 pos in
+
+  assert (size <= Bstr.length buffer && position <= size);
+
+  { buffer; position; size }
 
 let is_empty { position; size; _ } = position = size
+
+let sub ~off ~len { buffer; position; _ } =
+  Bstr.sub ~off:(off + position) ~len buffer
